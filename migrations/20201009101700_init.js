56ea6a8c69563801dc88aa5db09c8d0aa982c8bf
ajg7 <ajgebara@gmail.com>
2020-10-09 10:35:30 -0500

    added seed file and looked at database and has everything

--- migrations/20201009101700_init.js
+++ migrations/20201009101700_init.js
@@ -6,21 +6,21 @@ exports.up = function(knex) {
             table.string("name").notNullable().unique();
             table.string("description");
             table.boolean("completed").notNullable().defaultTo(false);
-            table.integer("taskID").unSigned().references("id").inTable("projects").onDelete("RESTRICT").onUpdate("CASCADE");
-            table.integer("resourceID").unSigned().references("id").inTable("resources").onDelete("RESTRICT").onUpdate("CASCADE");
+            table.integer("taskID").unsigned().references("id").inTable("projects").onDelete("RESTRICT").onUpdate("CASCADE");
+            table.integer("resourceID").unsigned().references("id").inTable("resources").onDelete("RESTRICT").onUpdate("CASCADE");
         })
         .createTable("tasks", table => {
             table.increments();
             table.string("description").notNullable();
             table.string("notes");
             table.boolean("completed").notNullable().defaultTo(false);
-            table.integer("projectID").unSigned().references("id").inTable("projects").onDelete("RESTRICT").onUpdate("CASCADE");
+            table.integer("projectID").unsigned().references("id").inTable("projects").onDelete("RESTRICT").onUpdate("CASCADE");
         })
         .createTable("resources", table => {
             table.increments();
             table.string("name").notNullable().unique();
             table.string("description");
-            table.integer("projectID").unSigned().references("id").inTable("projects").onDelete("RESTRICT").onUpdate("CASCADE");
+            table.integer("projectID").unsigned().references("id").inTable("projects").onDelete("RESTRICT").onUpdate("CASCADE");
         })
 };
 
