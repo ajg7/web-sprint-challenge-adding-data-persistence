dc0b494cb69306ac056a5c5f09bf0214baf19aba
ajg7 <ajgebara@gmail.com>
2020-10-09 10:32:35 -0500

    made migration file

--- migrations/20201009101700_init.js
+++ migrations/20201009101700_init.js
@@ -2,7 +2,25 @@
 exports.up = function(knex) {
     return knex.schema
         .createTable("projects", table => {
-
+            table.increments();
+            table.string("name").notNullable().unique();
+            table.string("description");
+            table.boolean("completed").notNullable().defaultTo(false);
+            table.integer("taskID").unSigned().references("id").inTable("projects").onDelete("RESTRICT").onUpdate("CASCADE");
+            table.integer("resourceID").unSigned().references("id").inTable("resources").onDelete("RESTRICT").onUpdate("CASCADE");
+        })
+        .createTable("tasks", table => {
+            table.increments();
+            table.string("description").notNullable();
+            table.string("notes");
+            table.boolean("completed").notNullable().defaultTo(false);
+            table.integer("projectID").unSigned().references("id").inTable("projects").onDelete("RESTRICT").onUpdate("CASCADE");
+        })
+        .createTable("resources", table => {
+            table.increments();
+            table.string("name").notNullable().unique();
+            table.string("description");
+            table.integer("projectID").unSigned().references("id").inTable("projects").onDelete("RESTRICT").onUpdate("CASCADE");
         })
 };
 
