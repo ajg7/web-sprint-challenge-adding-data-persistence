79de21a3206051a5ab2cb1d36f2e887c3a5e7c6e
ajg7 <ajgebara@gmail.com>
2020-10-09 10:55:21 -0500

    made tasks seed file

--- seeds/003-tasks.js
+++ seeds/003-tasks.js
@@ -1,13 +1,31 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  const tasks = [
+    {
+      description: "Make component",
+      notes: "",
+      completed: true
+    },
+    {
+      description: "Add action",
+      notes: "",
+      completed: true
+    },
+    {
+      description: "Add reducer",
+      notes: "",
+      completed: true
+    },
+    {
+      description: "Make an API request",
+      notes: "",
+      completed: true
+    },
+    {
+      description: "Install express",
+      notes: "",
+      completed: true
+    },
+  ]
+  return knex("tasks").insert(tasks);
 };
