2158d05289b12e3d9a426386d5971a70d99fa84c
ajg7 <ajgebara@gmail.com>
2020-10-09 10:23:14 -0500

    started creating the projects table and added the three tables to exports.down

--- migrations/20201009101700_init.js
+++ migrations/20201009101700_init.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
+    return knex.schema
+        .createTable("projects", table => {
 
+        })
 };
 
 exports.down = function(knex) {
-
-};
+    return knex.schema
+        .dropTableIfExists("projects")
+        .dropTableIfExists("resources")
+        .dropTableIfExists("tasks");
+}; 
